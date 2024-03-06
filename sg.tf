be1b05aeccb5ede6244712b26b5d0bb9243593d2
Marcello Gomes <marcellodeog@gmail.com>
2024-03-06 06:14:39 -0300

    Security groups - connection ports definition

---
+++ sg.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "mlflow_sg" {
+  name        = "mlflow_sg"
+  description = "Mlflow Security Group"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "mlflow_ui"
+    from_port        = var.mlflow_port
+    to_port          = var.mlflow_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "mlflow_sg"
+  }
+}
